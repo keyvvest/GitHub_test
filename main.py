05f6686b1ea9fbda5e01778f47a61b01f49bfdbb
Артём <keyvvest@icloud.com>
2025-03-21 00:52:31 +0300

    Добавлен файл main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+num = int(input("Введите число для вычисления факториала: "))
+print(f"Факториал числа {num} = {factorial(num)}")
